751304b58edd3ee75b76e5afd1163ead2d53a067
revprm <revy.pramana@gmail.com>
2025-05-12 13:07:11 +0700

    add example text file and comment out remote operations in client script

--- file_client_cli.py
+++ file_client_cli.py
@@ -92,4 +92,6 @@ def remote_delete(filename=""):
 if __name__ == "__main__":
     server_address = ("172.16.16.101", 6666)
     remote_list()
-    remote_get("donalbebek.jpg")
+    #remote_get("donalbebek.jpg")
+    #remote_upload("contoh.txt")
+    #remote_delete("contoh.txt")
